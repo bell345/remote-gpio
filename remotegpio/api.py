4083b4cb745b0a73825246e79d138072e82ec26f
Thomas Bell <tom.aus@outlook.com>
2022-06-26 19:22:11 +0800

    Output routes, updated requirements and service

--- remotegpio/api.py
+++ remotegpio/api.py
@@ -1,8 +1,14 @@
 """All of the endpoints are defined in this file."""
 
+from asyncio import sleep
+
 from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
 
+from RPi import GPIO
+GPIO.setmode(GPIO.BCM)
+
+from . import exceptions
 from .config import Settings, get_settings
 
 api = FastAPI()
@@ -22,3 +28,43 @@ async def hello(settings: Settings = Depends(get_settings)):
         'build_time': settings.build_time,
         'commit': settings.short_sha
     }
+
+
+@api.put('/gpio/{port}/on')
+async def turn_on(port: int):
+    if port < 0 or port > 53:
+        return exceptions.BadRequest.response(f'GPIO{port} is not a valid port')
+
+    GPIO.setup(port, GPIO.OUT)
+    GPIO.output(port, GPIO.HIGH)
+
+    return ''
+
+@api.put('/gpio/{port}/off')
+async def turn_off(port: int):
+    if port < 0 or port > 53:
+        return exceptions.BadRequest.response(f'GPIO{port} is not a valid port')
+
+    GPIO.setup(port, GPIO.OUT)
+    GPIO.output(port, GPIO.LOW)
+
+    return ''
+
+@api.put('/gpio/{port}/press')
+async def press(port: int,
+                delay_ms: int = 200,
+                settings: Settings = Depends(get_settings)):
+    if port < 0 or port > 53:
+        return exceptions.BadRequest.response(f'GPIO{port} is not a valid port')
+
+    if delay_ms < 0 or delay_ms > settings.max_press_delay:
+        return exceptions.BadRequest.response(
+            f'Press delay of {delay_ms} ms is over the limit of '+
+            f'{settings.max_press_delay} ms')
+
+    GPIO.setup(port, GPIO.OUT)
+    GPIO.output(port, GPIO.HIGH)
+    await sleep(delay_ms / 1000)
+    GPIO.output(port, GPIO.LOW)
+
+    return ''
--- remotegpio/config.py
+++ remotegpio/config.py
@@ -7,13 +7,9 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    aws_region: str = 'us-east-1'
     build_time: str = 'unknown'
     short_sha: str = 'manual'
-    media_bucket_name: str = 'media-bucket'
-    media_max_file_size: int = 50 * 1024 * 1024
-    media_user_quota: int = 1000
-    frontend_canonical_id: str = ''
+    max_press_delay: int = 60*60*1000
 
 
 @lru_cache
